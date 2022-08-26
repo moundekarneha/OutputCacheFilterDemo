a167bdde46a504ee584a07c917e1942622981746
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 15:51:00 +0530

    Add project files.

---
+++ OutputCacheFilterDemo/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutputCacheFilterDemo.Controllers
+{
+    //[OutputCache(Duration = 20)]
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        //time in se    cond
+        // [OutputCache(Duration = 20)]
+       // [Outp+utCache(Location = System.Web.UI.OutputCacheLocation.Any,Duration =20)]
+        [OutputCache(Location = System.Web.UI.OutputCacheLocation.Client,Duration =20)]
+        public ActionResult GetData()
+        {
+
+            return View();
+        }
+    }
+}
